e8cdf6cfe0c986393487157346bf2114b5419bcc
leen-gh <leen.ghaleb.d@gmail.com>
2023-12-22 17:15:20 +0300

    solved Assigment

---
+++ index.js
@@ -0,0 +1,17 @@
+function Scooter(year, color, model) {
+    this.model = model
+    this.year =year
+    this.color=color
+
+}
+
+function Driver(name, age, experience){
+    this.name=name
+    this.experience=experience
+    this.age=age
+}
+
+function PickupLocation(address, city){
+    this.city=city
+    this.address=address
+}
